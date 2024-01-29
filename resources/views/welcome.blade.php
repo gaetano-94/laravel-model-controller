bffefc52f52f77dfcdc0e487ca623deb20e90cb8
gaetano-94 <gaetano.panico1994@gmail.com>
2024-01-29 16:06:39 +0100

    feat: creazione controller e model

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,11 +5,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Laravel Template</title>
-      @vite('resources/js/app.js')
+    @vite('resources/js/app.js')
 </head>
 
 <body>
-     {{-- <img src="{{ Vite::asset('resources/img/duck.jpg') }}" alt=""> --}}
+    <div class="container py-5">
+        <div class="card" style="width: 18rem;">
+            <div class="card-body">
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                    card's content.</p>
+            </div>
+        </div>
+    </div>
 </body>
 
 </html>
